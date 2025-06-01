f727272465a7957b319ebdc69f27361e788a03ea
Sergio <serprigar@gmail.com>
2025-06-01 04:10:28 +0200

    feat(): create ProfileActivity

--- app/src/main/java/com/example/swop/profile/ProfileActivity.java
+++ app/src/main/java/com/example/swop/profile/ProfileActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import com.example.swop.BaseActivity;
 import com.example.swop.R;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    @Override protected int getLayoutResId() {return R.layout.activity_welcome;}
+
 }
