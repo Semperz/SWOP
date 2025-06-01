7a44f5bd6b2c836ef2f2fabd430f69387291c064
Sergio <serprigar@gmail.com>
2025-06-01 04:09:10 +0200

    feat(): finish WelcomeActivity

--- app/src/main/java/com/example/swop/WelcomeActivity.java
+++ app/src/main/java/com/example/swop/WelcomeActivity.java
@@ -1,13 +1,28 @@
 package com.example.swop;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.swop.shop.mainwindow.MainWindow;
 
 public class WelcomeActivity extends BaseActivity {
+    Button btnTienda, btnPujas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        initiazeUI();
+        btnTienda.setOnClickListener(v -> {
+            startActivity(new Intent(this, MainWindow.class));
+        });
     }
     @Override protected int getLayoutResId() {return R.layout.activity_welcome;}
+
+
+    private void initiazeUI() {
+         btnPujas = findViewById(R.id.btnBids);
+         btnTienda = findViewById(R.id.btnStore);
+    }
 }
